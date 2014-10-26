4bfa3538892aec74b4b88061a69b0660e995e638
Yuriy Kopylov <iyurij@gmail.com>
2014-10-26 18:20:43 +0600

    Added clear button for search fields.

---
+++ CramTool/CramTool/Views/SearchTextBox.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace CramTool.Views
+{
+    /// <summary>
+    /// Interaction logic for SearchTextBox.xaml
+    /// </summary>
+    public partial class SearchTextBox : UserControl
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text",
+            typeof (string),
+            typeof (SearchTextBox),
+            new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public SearchTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private void OnClearButtonPressed(object sender, RoutedEventArgs e)
+        {
+            UxTextBox.BeginChange();
+            try
+            {
+                UxTextBox.SelectAll();
+                UxTextBox.SelectedText = "";
+            }
+            finally
+            {
+                UxTextBox.EndChange();
+            }
+
+            UxTextBox.Focus();
+        }
+    }
+}
